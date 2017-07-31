739d10fd658e6d5488cfc6fcd7cb6cf4c80b919c
jebin007 <jevinsubedi@gmail.com>
2017-07-31 12:27:29 -0500

    Processing JSON Data To Show Employee Status:

--- stage 2/js/widget.js
+++ stage 2/js/widget.js
@@ -4,6 +4,18 @@ xhr. onreadystatechange = function () {
         var employees = JSON.parse(xhr.responseText);   //this line parses the regular string that the web browser grabs from the server and parses it to javascript.
         //When JSON.parse method is complete it returns a Javascript object.
         //the employees variable after the parsing becomes a Javascript object.
+        var statusHTML = '<ul class="bulleted">';
+        for (var i=0; i<employees.length; i += 1) {
+            if (employees[i].inoffice === true) {
+                statusHTML += '<li class="in">';
+            } else {
+                statusHTML += '<li class="out">';
+            }
+            statusHTML += employees[i].name;
+            statusHTML += '</li>';
+        }
+        statusHTML += '</ul>';
+        document.getElementById('employeeList').innerHTML = statusHTML;
     }
 };
 xhr.open('GET', 'data/employees.json');
